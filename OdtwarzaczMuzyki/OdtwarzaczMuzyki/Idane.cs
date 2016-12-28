9325b934e6c5dacdb4de8665548a92ea7ba2b36a
Paweł <karczewskipawel@hotmail.com>
2016-12-28 23:02:32 +0100

    Baza danych i interfejsy

---
+++ OdtwarzaczMuzyki/OdtwarzaczMuzyki/Idane.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdtwarzaczMuzyki
+{
+    interface Idane
+    {
+        void UtworzPlayliste(string nazwa);
+        void DodajDoPlaylisty(int idListy, List<string> listaUtworow);
+        void UsunZPlaylisty(int idListy, List<Utwor> listaUtworow);
+        List<string> PobierzPlaylisty(int idUzytkownika);
+        List<Utwor> PobierzUtworyZPlaylisty(int idPlaylist);
+        void UsunPlayliste(int idPlaylisty);
+    }
+}
---
+++ OdtwarzaczMuzyki/OdtwarzaczMuzyki/Ilogowanie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdtwarzaczMuzyki
+{
+    interface Ilogowanie
+    {
+        void UtworzKonto(Uzytkownik uzytkownik);
+        void Zaloguj(Uzytkownik uzytkownik);
+        void Wyloguj(Uzytkownik uzytkownik);
+        void ZresetujHasło(Uzytkownik uzytkownik);
+        void ZmienHasło(int idUzytkownika);
+    }
+}
