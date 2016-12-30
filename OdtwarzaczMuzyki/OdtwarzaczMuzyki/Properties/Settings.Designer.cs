fbdb132abd184d3479d4ffc47b52a236f22f9420
Monika <monikakarczmarczukk@gmail.com>
2016-12-30 13:38:39 +0100

    Baza po raz kolejny

--- OdtwarzaczMuzyki/OdtwarzaczMuzyki/Properties/Settings.Designer.cs
+++ OdtwarzaczMuzyki/OdtwarzaczMuzyki/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace OdtwarzaczMuzyki.Properties {
                 return ((string)(this["OMdatabaseConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database.mdf;" +
+            "Integrated Security=True")]
+        public string DatabaseConnectionString {
+            get {
+                return ((string)(this["DatabaseConnectionString"]));
+            }
+        }
     }
 }
